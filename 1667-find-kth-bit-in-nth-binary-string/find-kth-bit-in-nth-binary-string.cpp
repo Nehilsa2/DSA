9f09a71efbb8f1d0f19becfefe6328a40bfd7367
Nehil Sahu <nehilsahu281@gmail.com>
2026-03-03 12:33:56 +0530

    Time: 87 ms (17.18%) | Memory: 70.3 MB (23.91%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
@@ -6,11 +6,11 @@ public:
         string prev = "0";
         string curr = "0";
 
-        for(int ind = 2;ind<=n;ind++){
+        for(int ind = 2;ind<=n;ind++){  //0(n)
             
             curr += "1";
 
-            for(int i=prev.length()-1;i>=0;i--){
+            for(int i=prev.length()-1;i>=0;i--){ // o(len)
                 if(prev[i]=='0'){
                     curr+='1';
                 }
@@ -23,11 +23,24 @@ public:
             curr = prev;
         }
 
+        int required = k-1;
         int index = 0;
-        for(int i=0;i<curr.length();i++){
-            if(i==k-1) {
-                index = i;
+
+        int low = 0;
+        int high = curr.length()-1;
+
+        while(low<=high){
+            int mid = low + (high-low)/2;
+
+            if(mid==required){
+                index = mid;
                 break;
+            }
+            else if(mid>required){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
             }
         }
 
