3c2bcdbf462d74293c9dd5fb98622212317906f9
Nehil Sahu <nehilsahu281@gmail.com>
2026-03-06 00:45:26 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (97.53%) - LeetSync

--- 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
+++ 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
@@ -1,49 +1,18 @@
 class Solution {
 public:
     int countTriplets(vector<int>& arr) {
-        int n = arr.size();
-        vector<vector<int>>dp1(n,vector<int>(n,-1));
-        vector<vector<int>>dp2(n,vector<int>(n,-1));
-
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(j==i+1){
-                    dp1[i][j] = arr[i];
-                }
-                else{
-                    dp1[i][j] = dp1[i][j-1] ^ arr[j-1];
-                }
-            }
-        }
-
-        for(int i=1;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(j==i){
-                    dp2[i][j] = arr[i];
-                }
-                else{
-                    dp2[i][j] = dp2[i][j-1] ^ arr[j];
-                }
-            }
-        }
-
         int cnt = 0;
+        int n = arr.size();
 
         for(int i=0;i<n-1;i++){
+            int temp1 = arr[i];
             for(int j=i+1;j<n;j++){
-                for(int k=j;k<n;k++){
-                    int a = dp1[i][j];
-                    int b = dp2[j][k];
+                temp1 = temp1 ^ arr[j];
 
-                    if(a==b) cnt++;
-                }
+                if(temp1==0) cnt+=j-i;
             }
         }
 
         return cnt;
-
-        
     }
-
-    //[]
 };
