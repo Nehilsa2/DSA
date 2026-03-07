8d18d3bb0ad6df9f08044dbf84c3cc0d337d2928
Nehil Sahu <nehilsahu281@gmail.com>
2026-03-07 12:44:12 +0530

    Time: 50 ms (14.40%) | Memory: 28.3 MB (34.80%) - LeetSync

--- 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int minFlips(string s) {
         int n = s.length();
-        string t = s+s;
+        string newstr = s+s;
 
-        string alt0 = "" , alt1 = "";
+        string startwith0 = "" , startwith1 = "";
         for(int i=0;i<2*n;i++){
-            alt0+= (i%2==0) ? '0' : '1';
-            alt1+= (i%2==0) ? '1' : '0';
+            startwith0+= (i%2==0) ? '0' : '1';
+            startwith1+= (i%2==0) ? '1' : '0';
         }
 
 
@@ -18,13 +18,13 @@ public:
 
         for(int r = 0;r<2*n;r++){
             // for each window count the mismatch for both starting with 1 and 0 strings
-                if(alt0[r]!=t[r]) diff0++;
-                if(alt1[r]!=t[r]) diff1++;
+                if(startwith0[r]!=newstr[r]) diff0++;
+                if(startwith1[r]!=newstr[r]) diff1++;
 
 
                 if(r-l+1 > n){
-                    if(alt0[l]!=t[l]) diff0--;
-                    if(alt1[l]!=t[l]) diff1--;
+                    if(startwith0[l]!=newstr[l]) diff0--;
+                    if(startwith1[l]!=newstr[l]) diff1--;
                     l++;
                 }
 
