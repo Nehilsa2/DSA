174e7495e88055ba9d78c067df57138c97fb99da
Nehil Sahu <nehilsahu281@gmail.com>
2026-03-08 19:02:05 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (86.66%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -1,51 +1,18 @@
 class Solution {
 public:
-    int n;
-    int binarytodecimal(string s){
-        int ans = 0;
-
-        for(int i=0;i<n;i++){
-            ans += (1<<(n-i-1)) * (s[i]-'0');
-        }
-
-        return ans;
-    }
-
-    string decimaltobinary(int num){
-        string ans="";
-
-        while(num){
-            ans += (num%2)+'0';
-            num/=2;
-        }
-
-        reverse(ans.begin(),ans.end());
-
-        while(ans.size()<n) ans="0"+ans;
-
-        return ans;
-    }
-
     string findDifferentBinaryString(vector<string>& nums) {
-        unordered_map<int,int>mp;
-        n = nums.size();
+        string ans = "";
+        int n = nums.size();
 
         for(int i=0;i<n;i++){
-            int num = binarytodecimal(nums[i]);
-            mp[num]++;
-        }
-
-        int maxi = (1<<n) -1;
-
-        int number = 0;
-
-        for(int i=0;i<=maxi;i++){
-            if(mp.count(i)==0){
-                number = i;
-                break;
+            if(nums[i][i]=='0'){
+                ans+='1';
+            }
+            else{
+                ans+='0';
             }
         }
 
-        return decimaltobinary(number);
+        return ans;
     }
 };
