cbb86e607664cec915e80e90742b4cd220cdf52a
Nehil Sahu <nehilsahu281@gmail.com>
2026-03-26 22:40:56 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (75.40%) - LeetSync

---
+++ 476-number-complement/number-complement.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        int ans = 0;
+        int i = 0;
+        while(num!=0){
+            if((num&1)==0){
+                ans += (1<<i);
+            }
+            i++;
+            num = num>>1;
+        }
+
+        return ans;
+
+    }
+
+    //101
+};
