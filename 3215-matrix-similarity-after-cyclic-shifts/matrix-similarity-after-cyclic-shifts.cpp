2005edc13407cd40d0cdd01b0588e1375dec651d
Nehil Sahu <nehilsahu281@gmail.com>
2026-03-27 10:37:13 +0530

    Time: 3 ms (30.43%) | Memory: 32.5 MB (33.54%) - LeetSync

---
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void rightShift(vector<int>&v,int m,int k){
+        for(int i=0;i<k;i++){
+            reverse(v.begin(),v.begin() + (v.size()-1));
+            reverse(v.begin(),v.end());
+        }
+    }
+
+    void leftShift(vector<int>&v,int m,int k){
+        for(int i=0;i<k;i++){
+            reverse(v.begin()+1,v.end());
+            reverse(v.begin(),v.end());
+        }
+    }
+
+    bool areSimilar(vector<vector<int>>& mat, int k) {
+        int n = mat.size();
+        int m = mat[0].size();
+
+        vector<vector<int>>temp = mat;
+        
+        for(int i=0;i<n;i++){ //(n)
+            if(i&1){
+                rightShift(temp[i],m,k); // o(k*m)
+            }
+            else{
+                leftShift(temp[i],m,k);
+            }
+        }
+
+        return (temp==mat);
+    }
+};
