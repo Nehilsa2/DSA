c38400a4fef3db475dce3541427a8a6e90492121
Nehil Sahu <nehilsahu281@gmail.com>
2026-04-11 01:28:37 +0530

    Time: 1 ms (39.92%) | Memory: 15.5 MB (76.59%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -24,7 +24,32 @@ public:
         int n = grid.size();
         int m = grid[0].size();
 
-        memset(dp,-1,sizeof(dp));
-        return solve(n-1,m-1,grid);
+        memset(dp,0,sizeof(dp));
+
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 && j==0){
+                    dp[0][0] = grid[0][0];
+                    continue;
+                }
+
+                int up = INT_MAX;
+                int left = INT_MAX;
+
+                if(i>0){
+                    up = dp[i-1][j];
+                }
+                if(j>0){
+                    left = dp[i][j-1];
+                }
+
+                dp[i][j] = grid[i][j] + min(left,up);
+
+            }
+        }
+
+        // return solve(n-1,m-1,grid);
+        return dp[n-1][m-1];
     }
 };
